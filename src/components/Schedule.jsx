17ba63b35284689a0932d76d83fbefa22a1b3120
Emilia Depka <rockycentralparty@gmail.com>
2024-11-19 12:55:53 +0100

    Update Schedule.jsx
    Aktualizacja harmonogramu.

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -20,7 +20,8 @@ const Schedule = () => {
         { date: 'Spotkanie 6', description: 'Określenie, w jakich technologiach zostanie stworzony projekt. Podział ról. Stworzenie repozytorium na githubie. Stworzenie prostej bazy danych, frontendu oraz backendu formularza zgłoszeniowego.' },
         { date: 'Spotkanie 7', description: 'Prezentacja pierwszej prostej wersji aplikacji. Ustalenie dalszych planów na rozwój aplikacji. Aktualizacja bazy danych, frontendu oraz backendu o możliwość logowania użytkowników z różnymi poziomami dostępów.' },
         { date: 'Spotkanie 8', description: 'Dodanie możliwości przeglądania zgłoszeń przez administratorów, dodatkowe podstrony, haszowanie haseł, poprawa frontendu, aktualizacja bazy danych.' },
-        { date: 'Spotkanie 9', description: 'Aktualizacja bazy danych oraz formularza zgłoszeniowego. Decyzja o nazwie aplikaci - Szkolny Bezpiecznik.' },
+        { date: 'Spotkanie 9', description: 'Aktualizacja bazy danych oraz formularza zgłoszeniowego. Decyzja o nazwie aplikacji - Szkolny Bezpiecznik.' },
+        { date: 'Spotkanie 10', description: 'Rozszerzenie bazy danych o dodatkowe kolumny. Modyfikacja formularza zgłoszeniowego o możliwość pobierania z tabeli szkoly danych przy pomocy zapytania SELECT. Dodanie strony do zarządzania administratorami, dodanie możliwości filtrowania i sortowania użytkowników oraz zgłoszeń. Dodanie strony ze szczegółami danego zgłoszenia oraz szkoły. Aktualizacja strony internetowej. Przepisanie całości aplikacji, by korzystała z React-Bootstrap oraz dostosowanie stylizacji stron.' }
 
     ];
 
